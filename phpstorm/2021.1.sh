7f55aae2f26fb4b532ca4553b92db6a9b14d0679
tuansacca <tuansacca@gmail.com>
2022-08-01 23:24:50 +0700

    Reset PhpStorm2021.1 trial

---
+++ phpstorm/2021.1.sh
@@ -0,0 +1,5 @@
+sudo rm -rf ~/.java/.userPrefs/jetbrains/phpstorm
+sudo rm -rf ~/.config/JetBrains/PhpStorm2021.1/eval/*
+sudo rm -rf ~/.config/JetBrains/PhpStorm2021.1/options/other.xml
+sudo rm -rf ~/.config/JetBrains/PhpStorm2021.1/eval/*
+sudo rm -rf ~/.config/JetBrains/PhpStorm2021.1/options/other.xml
